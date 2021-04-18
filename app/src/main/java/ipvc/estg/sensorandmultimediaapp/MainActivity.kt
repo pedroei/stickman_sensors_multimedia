cb0c76decf370580e7d23ddda488c306a40e5d4c
DESKTOP-VQ33N6D\pedro <pedro.araujo@atlanse.pt>
2021-04-18 15:44:00 +0100

    Implemented light sensor

--- app/src/main/java/ipvc/estg/sensorandmultimediaapp/MainActivity.kt
+++ app/src/main/java/ipvc/estg/sensorandmultimediaapp/MainActivity.kt
@@ -1,8 +1,13 @@
 package ipvc.estg.sensorandmultimediaapp
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import android.opengl.Visibility
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Build
 import android.os.Bundle
 import android.speech.RecognizerIntent
@@ -12,14 +17,24 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.io.IOException
 import java.util.*
 
-class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener, SensorEventListener {
 
     private var tts: TextToSpeech? = null
     private val RQ_SPEECH_REC = 102
+
+    var lightSensor: Sensor? = null
+    var sensorManager: SensorManager? = null
+
+    lateinit var ivStickman: ImageView
+    lateinit var btnSpeech: Button
+    lateinit var ivStickmanSleep: ImageView
+    lateinit var rlMain: RelativeLayout
 
     //TODO: Sensor de luz
     //TODO: Abanar o telemovel
@@ -31,12 +46,16 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
         tts = TextToSpeech(this, this)
 
-        val ivStickman: ImageView = findViewById(R.id.ivStickman)
-        val btnSpeech: Button = findViewById(R.id.btnSpeech)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        lightSensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
+
+        ivStickman = findViewById(R.id.ivStickman)
+        btnSpeech = findViewById(R.id.btnSpeech)
+        rlMain = findViewById(R.id.rlMain)
 
         ivStickman.setOnClickListener {view ->
-            speakOut("Welcome!")
-            speakOut("What's your name?")
+            speakOut("Welcome!", TextToSpeech.QUEUE_ADD)
+            speakOut("What's your name?", TextToSpeech.QUEUE_ADD)
             btnSpeech.isClickable = true
             btnSpeech.visibility = View.VISIBLE
         }
@@ -52,7 +71,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
         if (requestCode == RQ_SPEECH_REC && resultCode == Activity.RESULT_OK) {
             val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-            speakOut("Hello $result")
+            speakOut("Hello $result", TextToSpeech.QUEUE_ADD)
         }
     }
 
@@ -68,9 +87,9 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         }
     }
 
-    private fun speakOut(text: String) {
+    private fun speakOut(text: String, type: Int) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            tts!!.speak(text, TextToSpeech.QUEUE_ADD, null, "")
+            tts!!.speak(text, type, null, "")
         } else {
             Toast.makeText(this, "Your android version does not support this", Toast.LENGTH_SHORT).show()
         }
@@ -97,5 +116,33 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         }
 
         super.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager!!.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager!!.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        //Not implemented for light sensor
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        try {
+            if (event!!.values[0] < 10) {
+                ivStickman.setImageResource(R.drawable.stickman_sleep)
+                rlMain.setBackgroundColor(Color.parseColor("#cccccc"))
+                speakOut("It's a beautiful night...", TextToSpeech.QUEUE_FLUSH)
+            } else {
+                ivStickman.setImageResource(R.drawable.stickman_hello)
+                rlMain.setBackgroundColor(Color.parseColor("#ffffff"))
+
+            }
+        } catch(e: IOException){}
     }
 }
